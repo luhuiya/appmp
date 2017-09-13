313b9069fd83b2cc56ca1cbcbc6c6590a7bdac89
I Komang Suhendra Eka Putra <IKomangSuhendra.EkaPutra@mitrais.com>
2017-09-13 16:17:12 +0800

    Add chart model and migration

---
+++ backend/app/Chart.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace onestopcore;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Chart extends Model
+{
+    //
+}
---
+++ backend/database/migrations/2017_09_13_075656_create_charts_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChartsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('charts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->decimal('total_price');
+            $table->integer('total_products');
+            $table->boolean('is_active')->default(false);
+            $table->integer('user_id');
+            $table->timestamps();
+        });
+
+        Schema::table('charts', function (Blueprint $table) {
+            $table->foreign('user_id', 'fk_charts_user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('charts');
+    }
+}
