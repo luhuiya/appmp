06f2a2511e082d11e900f3de6573c42725082e73
I Komang Suhendra Eka Putra <IKomangSuhendra.EkaPutra@mitrais.com>
2017-09-13 14:41:41 +0800

    Modify transaction table, set relationship to user

--- backend/app/Transaction.php
+++ backend/app/Transaction.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
-    //
+    protected $fillable = [
+        'transaction_code',
+        'user_id',
+    ];
+
+
+    /**
+     * Get the user that owns the transaction.
+     */
+    public function user()
+    {
+        return $this->belongsTo('onestopcore\User');
+    }
 }
