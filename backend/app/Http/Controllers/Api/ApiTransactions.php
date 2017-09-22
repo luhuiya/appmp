331206f8c8a92446b2eb33aafc5b39b8698f3acb
I Komang Suhendra Eka Putra <IKomangSuhendra.EkaPutra@mitrais.com>
2017-09-22 17:30:25 +0800

    Initial transaction

---
+++ backend/app/Http/Controllers/Api/ApiTransactions.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace onestopcore\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use onestopcore\Http\Controllers\Controller;
+use onestopcore\Transaction;
+
+class ApiTransactions extends Controller
+{
+    public function getById($transactionId)
+    {
+        $transaction = Transaction::with('details.product')->find($transactionId);
+        if ($transaction)
+        {
+            return response()->json($transaction, 200);
+        }
+
+        return response()->json([
+            'error' => true,
+            'message'   => 'The transaction is not exist',
+        ], 400);
+    }
+}
